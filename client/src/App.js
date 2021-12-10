06b13c74a504a770960ee6acfb53744a803d6d50
Matthew Nguyen <matthewnguyen0814@gmail.com>
2021-12-10 07:02:57 -0600

    fixed app.js layout

--- client/src/App.js
+++ client/src/App.js
@@ -14,24 +14,17 @@ import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 import { Provider } from 'react-redux';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+})
 
 function App() {
   return (
