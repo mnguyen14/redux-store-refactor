883339adb2cce7de8d1d4bdd53209030f1061006
Matthew Nguyen <matthewnguyen0814@gmail.com>
2021-12-10 06:57:27 -0600

    added redux to app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
@@ -14,9 +9,10 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+import store from './utils/store';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
+import { Provider } from 'react-redux';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -42,7 +38,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -53,7 +49,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
